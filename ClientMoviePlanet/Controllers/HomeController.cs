873aedac9ef2f8779177e6e9e1105301f6cff587
Gurkirat Singh Khaira <62628582+khaira777@users.noreply.github.com>
2022-11-24 06:30:15 -0500

    Simplified(Fixed) the Home(Company)Controller
    removed multiple instantiations of HttpClient object

--- ClientMoviePlanet/Controllers/HomeController.cs
+++ ClientMoviePlanet/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
 ﻿using ClientMoviePlanet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Diagnostics;
 using System.Net.Http.Headers;
 
 namespace ClientMoviePlanet.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient _httpClient = new HttpClient();
         //Hosted web API REST Service base url
-        string Baseurl = "http://movieplanetapi-766262829.us-east-1.elb.amazonaws.com/";
+        private string uri = "http://movieplanetapi-766262829.us-east-1.elb.amazonaws.com/";
+        private HttpResponseMessage response;
+
+        public HomeController()
+        {
+            _httpClient.BaseAddress = new Uri(uri);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
 
         public async Task<ActionResult> Index(int companyId)
         {
@@ -18,75 +26,42 @@ namespace ClientMoviePlanet.Controllers
             // if company id is selected (show company with that id)
             if (companyId != null && companyId != 0)
             {
-                Trace.WriteLine("Company ID: " + companyId);
-  
-                CompanyInfo companyInfo = new CompanyInfo();
-                using (var client = new HttpClient())
-                {
-                    //Passing service base url
-                    client.BaseAddress = new Uri(Baseurl);
-                    client.DefaultRequestHeaders.Clear();
-                    //Define request data format
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient
-                    HttpResponseMessage Res = await client.GetAsync("api/CompanyInfo/" + companyId);
-                    //Checking the response is successful or not which is sent using HttpClient
-                    if (Res.IsSuccessStatusCode)
-                    {
-                        //Storing the response details recieved from web api
-                        var companyResponse = Res.Content.ReadAsStringAsync().Result;
-                        //Deserializing the response recieved from web api and storing into the Employee list
-                        companyInfo = JsonConvert.DeserializeObject<CompanyInfo>(companyResponse);
-                        companyInfoList.Add(companyInfo);
-                    }
-                    //returning the employee list to view
-                    return View(companyInfoList);
-                }
-            }
+                //Sending request to find web api REST service resource GetAllCompanies using HttpClient
+                response = await _httpClient.GetAsync("api/CompanyInfo/" + companyId);
 
-            // if no company id is selected (show all companies)
-            using (var client = new HttpClient())
-            {
-                //Passing service base url
-                client.BaseAddress = new Uri(Baseurl);
-                client.DefaultRequestHeaders.Clear();
-                //Define request data format
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
-                HttpResponseMessage Res = await client.GetAsync("api/companyInfos");
                 //Checking the response is successful or not which is sent using HttpClient
-                if (Res.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
-                    //Storing the response details recieved from web api
-                    var companyResponse = Res.Content.ReadAsStringAsync().Result;
-                    //Deserializing the response recieved from web api and storing into the Employee list
-                    companyInfoList = JsonConvert.DeserializeObject<List<CompanyInfo>>(companyResponse);
+                    CompanyInfo companyInfo = new CompanyInfo();
+
+                    //Storing the response details received from web api
+                    var companyResponse = response.Content.ReadAsStringAsync().Result;
+
+                    // Deserializing the response received from web api and storing into the Company list
+                    companyInfo = JsonConvert.DeserializeObject<CompanyInfo>(companyResponse);
+                    companyInfoList.Add(companyInfo);
                 }
-                //returning the employee list to view
+                //returning the company list to view
                 return View(companyInfoList);
             }
 
-            //private readonly ILogger<HomeController> _logger;
-
-            //public HomeController(ILogger<HomeController> logger)
-            //{
-            //    _logger = logger;
-            //}
-
-            //public IActionResult Index()
-            //{
-            //    return View();
-            //}
+            // if no company id is selected or 0 (show all companies)
+            //Sending request to find web api REST service resource GetAllCompanies using HttpClient
+            response = await _httpClient.GetAsync("api/companyInfos");
+            //Checking the response is successful or not which is sent using HttpClient
+            if (response.IsSuccessStatusCode)
+            {
+                //Storing the response details received from web api
+                var companyResponse = response.Content.ReadAsStringAsync().Result;
+                //Deserializing the response recieved from web api and storing into the Employee list
+                companyInfoList = JsonConvert.DeserializeObject<List<CompanyInfo>>(companyResponse);
+            }
+            //returning the employee list to view
+            return View(companyInfoList);
         }
         public IActionResult Privacy()
         {
            return View();
         }
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
     }
 }
